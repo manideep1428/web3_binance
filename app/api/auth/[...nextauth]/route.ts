c5925148de3f12b61a568d83bcc396c4ac5c8e9f
manideep1428 <saimanideep.ch12345@gmail.com>
2024-08-28 12:35:49 +0530

    Updated Schema

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,9 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import prisma from "@/db"
+import { PrismaClient } from "@prisma/client"
+
+const prisma  = new PrismaClient();
 
 const handler = NextAuth({
   session:{
