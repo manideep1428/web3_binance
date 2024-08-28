d694bbff3a47a903172d0f665783252a7c9d139d
manideep1428 <saimanideep.ch12345@gmail.com>
2024-08-28 12:21:47 +0530

    Added Features

--- db/prisma/migrations/20240711050148_init4/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `date` on the `Order` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Order" DROP COLUMN "date";
